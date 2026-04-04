50c0b45fb52defb0f5a8b616c89f4b6c18887c9c
Tejaswaroop25 <tejaswaroop367@gmail.com>
2026-04-04 23:09:09 +0530

    Capture final manual content edits

--- frontend/script.js
+++ frontend/script.js
@@ -87,9 +87,7 @@ function checkAuthState() {
     }
 
     if (userAuth && userAuth.loggedIn && loginBtn) {
-        const isMobile = window.innerWidth < 600;
-        loginBtn.innerHTML = `<i class="fa-solid fa-right-from-bracket"></i> ${isMobile ? 'Logout' : 'Logout (' + userAuth.name + ')'}`;
-
+        loginBtn.innerHTML = `<i class="fa-solid fa-right-from-bracket"></i> Logout (${userAuth.name})`;
         loginBtn.classList.remove('btn-login');
         loginBtn.classList.add('btn-logout');
         loginBtn.href = "#";
