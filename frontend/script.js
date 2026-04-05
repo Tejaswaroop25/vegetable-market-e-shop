148a71345e86b291187ac48be62e336a21f7238e
Tejaswaroop25 <tejaswaroop367@gmail.com>
2026-04-05 15:55:15 +0530

    final: refined search bar layout and other interface polish

--- frontend/script.js
+++ frontend/script.js
@@ -87,10 +87,9 @@ function checkAuthState() {
     }
 
     if (userAuth && userAuth.loggedIn && loginBtn) {
-        loginBtn.innerHTML = `<i class="fa-solid fa-sign-out-alt"></i> Logout`;
+        loginBtn.innerHTML = `<i class="fa-solid fa-right-from-bracket"></i> Logout (${userAuth.name})`;
         loginBtn.classList.remove('btn-login');
         loginBtn.classList.add('btn-logout');
-        loginBtn.title = `Logged in as ${userAuth.name}`; // Keep full name in tooltip
         loginBtn.href = "#";
         loginBtn.onclick = (e) => {
             e.preventDefault();
